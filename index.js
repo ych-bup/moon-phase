c25ab99faaa044568c36beb94071224a0173836d
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-30 15:20:55 +0900

    d

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const prefix = 'm?';
 
+const moon = require('celestial-moon');
+
 client.on('ready', async () => {
     console.log('We logged in as ' + client.user.tag + '!');
     client.user.setActivity(`what is today's moon phase?`,{ type: "STREAMING" });
@@ -10,7 +12,8 @@ client.on('ready', async () => {
 
 client.on('message', async message => {
     if(message.content == prefix + 'help'){
-        message.channel.send('wait.');
+        const phase = moon.calculate(2020, 11, 10, 9, 30, 0, 0);
+        message.channel.send(phase.name);
     }    
 });
 
