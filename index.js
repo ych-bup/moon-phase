7e49ae41f57f50ab196ee2c119d7b2dddbe39a06
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-31 11:58:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,62 +17,63 @@ client.on('message', async message => {
     var hour = new Date().getHours()
     var minute = new Date().getMinutes()
     var second = new Date().getSeconds()
+    var fullDate = new Date.now();
 
     const phase = moon.calculate(year, month, date, hour, minute, second, 0);
     
     const newMoon = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **NEW MOON**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **NEW MOON**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/d/dd/New_Moon.jpg') // finish
         .setTimestamp()
     
     const waxingCrescent = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **WAXING CRESCENT**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **WAXING CRESCENT**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://i1.wp.com/awesomesasquatch.com/wp-content/uploads/2014/06/waxing-crescent_9786.jpg?resize=1600%2C1600') // finish
         .setTimestamp()
     
     const firstQuarter = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **FIRST QUARTER**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **FIRST QUARTER**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://sites.google.com/site/moonfactssciencelesson/_/rsrc/1334676428877/the-phases-of-the-moon/first-quarter/first%20quarter.jpg') // finish
         .setTimestamp()
     
     const waxingGibbous = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **WAXING GIBBOUS**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **WAXING GIBBOUS**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://3.bp.blogspot.com/-0rmUmbNUJ2Q/VdGLlWPAMqI/AAAAAAAAKVU/r9gCl2SOfFU/s1600/moon_4_14_00.jpg') // finish
         .setTimestamp()
     
     const fullMoon = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **FULL MOON**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **FULL MOON**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Supermoon_Nov-14-2016-minneapolis.jpg/1200px-Supermoon_Nov-14-2016-minneapolis.jpg') // finish
         .setTimestamp()
     
     const waningGibbous = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **WANING GIBBOUS**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **WANING GIBBOUS**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://www.enchantedlearning.com/shimmerings/images/Moon-phase718waning-20170911-0736utc-A73E0551-M.png') // finish
         .setTimestamp()
         
     const lastQuarter = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **LAST QUARTER**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **LAST QUARTER**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/last-quarter-moon.jpg?itok=yMQmAF5f') // finish 
         .setTimestamp()
     
     const waningCrescent = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('***TODAY\'s MOON PHASE!!***')
-        .setDescription(`Today's moon phase is **WANING CRESCENT**.\n\n\`\`utc +0\`\``)
+        .setDescription(`Today's moon phase is **WANING CRESCENT**.\n\n\`\`${fullDate} utc +0\`\``)
         .setThumbnail('https://sites.google.com/site/moonfactssciencelesson/_/rsrc/1334592105347/the-phases-of-the-moon/waning-crescent/waning%20crescent.jpg?height=272&width=274') // finish
         .setTimestamp()
     
